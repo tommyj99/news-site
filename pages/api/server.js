71524c7ca16bbced65eba5bb785a0f51e6fb01ad
Thomas Jacques <jacquestom99@gmail.com>
2022-04-07 15:42:27 -0700

    added cors code from next website

--- pages/api/server.js
+++ pages/api/server.js
@@ -1,5 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import axios from "axios";
+import Cors from "cors";
+import initMiddleware from "../../utils/init-middleware";
+
+// Initialize the cors middleware
+const cors = initMiddleware(
+  // You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+  Cors({
+    // Only allow requests with GET, and OPTIONS
+    methods: ["GET", "OPTIONS"],
+  })
+);
 
 export default async (req, res) => {
   let mainChoice = req.query.mainchoice;
@@ -12,6 +23,8 @@ export default async (req, res) => {
   } else {
     url = `https://newsapi.org/v2/${mainChoice}?language=en&from=${date}&apiKey=${process.env.NEWS_KEY}`; // process.env.REACT_APP_NEWS_API_KEY2
   }
+
+  await cors(req, res);
 
   await axios.get(url).then(({ data }) => {
     res.status(200).json({ data });
