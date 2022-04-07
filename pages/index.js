2942c5816584b14bcf98be9daacefeb490201ba8
Thomas Jacques <jacquestom99@gmail.com>
2022-04-06 17:10:07 -0700

    fixed alignment

--- pages/index.js
+++ pages/index.js
@@ -31,7 +31,7 @@ export default function Home() {
         </Box>
         <Box
           style={{
-            marginLeft: "5%",
+            marginLeft: "1%",
             marginRight: "5%",
           }}
         >
